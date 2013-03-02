336d0c73e7c1eb5fac4850fdedb96ce9d961a5cf
Daniel Gillis <dtgillis@cs.unc.edu>
2013-03-02 15:10:03 -0500

    Added hap class but not finished relies heavily on demography classes

---
+++ src/haplos/hap.java
@@ -0,0 +1,18 @@
+package haplos;
+
+import pop.population;
+
+public class hap {
+	int npop,popid,nchrom,whichPopIndex,mutIndex,nMut,mutArraySize;
+	final int INIT_SIZE = 100;
+	population aPop;
+	public hap(){
+		
+		
+	}
+	public void hapAssignChroms(){
+		int running = 0;
+		//npop = 
+		
+	}
+}
