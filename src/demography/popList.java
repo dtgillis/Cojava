d4d62f52488c00ee55171128749452282f8d2c2d
Daniel Gillis <dtgillis@cs.unc.edu>
2013-03-02 16:15:48 -0500

    objects needed for the demography object

---
+++ src/demography/popList.java
@@ -0,0 +1,26 @@
+package demography;
+
+import pop.population;
+
+public class popList {
+	population pop;
+	popList next;
+	 
+	public popList(){
+		
+	}
+	
+	public population getPop(){
+		return pop;
+	}
+	public void setPop(population aPop){
+		pop = aPop;
+	}
+	public popList getNext(){
+		return next;
+	}
+	public void setNext(popList aNext){
+		next = aNext;
+	}
+
+}
