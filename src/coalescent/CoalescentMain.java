bc21888ea5c775bc2dba1f6de74993f1d108602e
Daniel Gillis <dtgillis@cs.unc.edu>
2013-03-03 22:55:45 -0500

    Added a few things for the demography class 	Also made it static so we can get to it from other classes also 	we do not want to declare it anywhere else.....

--- src/coalescent/CoalescentMain.java
+++ src/coalescent/CoalescentMain.java
@@ -1,15 +1,22 @@
 package coalescent;
 
+import demography.demography;
 import files.fileReader;
 
 public class CoalescentMain {
 	static ArgHandler fileHolder;
 	static fileReader inputHandler;
+	public static demography dem;
 	public static void main(String[] args) {
 		fileHolder = new ArgHandler(args);
 		fileHolder.setArguments();
+		dem = new demography();
+		dem.setLogFile(fileHolder.getLogFile());
+		dem.initRecomb();
+		
 		inputHandler = new fileReader(fileHolder);
 		inputHandler.paramFileProcess();
+		
 		
 	}
 
