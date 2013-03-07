46fe16e698971d80a294e10e5ac7548caad7110a
Daniel Gillis <dtgillis@cs.unc.edu>
2013-03-07 10:21:22 -0500

    pos is an array of doubles not a double.....

--- src/mutate/mutList.java
+++ src/mutate/mutList.java
@@ -3,12 +3,13 @@ package mutate;
 public class mutList {
 	final int INITSIZE = 500;
 	int nMut;
-	double pos;
+	double[] pos;
 	int arraySize;
 
 	public mutList(){
 		nMut = 0;
 		arraySize = INITSIZE;
+		pos = new double[INITSIZE];
 		
 	}
 	public void setNumMut(int numMut){
@@ -17,11 +18,20 @@ public class mutList {
 	public int getNumMut(){
 		return nMut;
 	}
-	public void setPos(double aPos){
+	public void setPos(double[] aPos){
 		pos = aPos;
+	}
+	public void setPos(int index,double data){
+		pos[index] = data;
+	}
+	public int getArraySize(){
+		return arraySize;
 	}
 	public void setArraySize(int anArraySize){
 		arraySize = anArraySize;
+	}
+	public int getNMut(){
+		return nMut;
 	}
 	
 }
