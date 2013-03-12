1f2985be2a7c5174599c2c57914cf5c29483b851
Daniel Gillis <dtgillis@cs.unc.edu>
2013-03-11 22:18:04 -0400

    add a new pointer integer and double object....

---
+++ src/pointers/doublePointer.java
@@ -0,0 +1,14 @@
+package pointers;
+
+public class doublePointer {
+	double d;
+	public doublePointer(){
+		d = 0;
+	}
+	public double getDouble(){
+		return d;
+	}
+	public void setDouble(double aDouble){
+		d = aDouble;
+	}
+}
---
+++ src/pointers/intPointer.java
@@ -0,0 +1,14 @@
+package pointers;
+
+public class intPointer {
+	int d;
+	public intPointer(){
+		d = 0;
+	}
+	public int getInt(){
+		return this.d;
+	}
+	public void setInt(int anInt){
+		this.d = anInt;
+	}
+}
